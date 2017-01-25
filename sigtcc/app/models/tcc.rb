c6e85b86c2e54d0ac4f5c15f2468efe3a471eb52
mayermatheus <matheusmayerduarte@gmail.com>
2017-01-24 23:26:13 -0300

    [UPDATE] - Adicionando enum no tcc

--- sigtcc/app/models/tcc.rb
+++ sigtcc/app/models/tcc.rb
@@ -1,2 +1,3 @@
-class Tcc < ApplicationRecord
+class Tcc < ActiveRecord::Base
+  enum tipo: {estagio: 1, pesquisa: 2, trabalhador: 3}
 end
---
+++ sigtcc/db/migrate/20170124172942_create_tccs.rb
@@ -0,0 +1,13 @@
+class CreateTccs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tccs do |t|
+      t.string :titulo
+      t.string :periodo
+      t.string :palavras_chaves, array: true
+      t.string :tema
+      t.column :tipo, :integer, default: 1
+
+      t.timestamps
+    end
+  end
+end
