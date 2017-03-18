4b729fe8a185196a15d6a7adb5efa68de0071506
samyralara <samyralara@outlook.com>
2017-03-17 23:48:55 -0300

    adicionando funcionalidade e teste com Capybara

--- sigtcc/spec/views/usuarios/show.html.erb_spec.rb
+++ sigtcc/spec/views/usuarios/show.html.erb_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "usuarios/show", type: :view do
-	
-	tiago = Aluno.create(matricula: "20141370526")
-	Usuario.create(user: tiago, nome: "Tiago Cesario",email: "tiago@tiago.com", password: "1111", password_confirmation: "1111", ativo: true, sexo: "masculino")
+ context 'testando' do
+ 	it 'testando' do
 
-	heremita = Professor.create(siape: "12345678")
-	Usuario.create(user: valeria, nome: "Heremita Lira",email: "heremita@heremita.com", password: "1111", password_confirmation: "1111", ativo: true, sexo: "feminino")
+		tiago = Aluno.create(matricula: "20141370526")
+		Usuario.create(user: tiago, nome: "Tiago Cesario",email: "tiago@tiago.com", password: "1111", password_confirmation: "1111", ativo: true, sexo: "masculino")
 
-	PropostaTcc.create(dataApresentacao: "28/02/2017", aprovacao: false, arquivo: File.new(Rails.root + 'spec/test_upload/dadosabertos.pdf'), aluno: tiago)
+		heremita = Professor.create(siape: "12345678")
+		Usuario.create(user: heremita, nome: "Heremita Lira",email: "heremita@heremita.com", password: "1111", password_confirmation: "1111", ativo: true, sexo: "feminino")
 
-	Tcc.create(titulo: "TCC de Tiago", periodo: "2016.2", agendad: true, tipos: 2, professor: heremita, arquivo: File.new(Rails.root + 'spec/test_upload/dadosabertos.pdf'), nota: 10, status: "aprovado", proposta_tcc: tiago.proposta_tcc)
+		PropostaTcc.create(dataApresentacao: "28/02/2017", aprovacao: false, arquivo: File.new(Rails.root + 'spec/test_upload/dadosabertos.pdf'), aluno: tiago)
 
-	Banca.create(dataApresentacao: "24/05/2017", horario: Date.new, tcc: tiago.proposta_tcc.tcc)
+		Tcc.create(titulo: "TCC de Tiago", periodo: "2016.2", agendad: true, tipos: 2, professor: heremita, arquivo: File.new(Rails.root + 'spec/test_upload/dadosabertos.pdf'), nota: 10, status: "aprovado", proposta_tcc: tiago.proposta_tcc)
+
+		Banca.create(dataApresentacao: "24/05/2017", horario: Date.new, tcc: tiago.proposta_tcc.tcc)
+	visit "/login"
+	fill_in 'Email', with: 'tiago@tiago.com'
+	fill_in 'Password', with: '1111'
+	click_button 'Acessar'
+	page.has_checked_field?('status-defendido')
+end
+end
+
