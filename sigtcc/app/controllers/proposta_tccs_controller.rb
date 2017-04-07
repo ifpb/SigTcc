59c7f5fce21b620ea0bbad6fe422d0a0898c8855
tiagocesariob <tiagocesariob@hotmail.com>
2017-04-07 14:25:30 -0300

    concluido proposta tcc

--- sigtcc/app/controllers/proposta_tccs_controller.rb
+++ sigtcc/app/controllers/proposta_tccs_controller.rb
@@ -25,11 +25,12 @@ class PropostaTccsController < ApplicationController
   # POST /proposta_tccs.json
   def create
     #@proposta_tcc = PropostaTcc.new(proposta_tcc_params)
-    @proposta_tcc = PropostaTcc.new()
-    @proposta_tcc.dataApresentacao = params[:proposta_tcc][:dataApresentacao]
-    @proposta_tcc.tcc.titulo = params[:titulo]
-
+    @proposta_tcc = PropostaTcc.new(proposta_tcc_params)
+  #  @proposta_tcc.dataApresentacao = params[:proposta_tcc][:dataApresentacao]
+    #@proposta_tcc.aluno_id = params[:proposta_tcc][:aluno_id]
   #  @proposta_tcc.aprovacao = params[:aprovacao]
+
+
 
     respond_to do |format|
       if @proposta_tcc.save
@@ -45,11 +46,6 @@ class PropostaTccsController < ApplicationController
   # PATCH/PUT /proposta_tccs/1
   # PATCH/PUT /proposta_tccs/1.json
   def update
-
-    @proposta_tcc = PropostaTcc.new()
-    @proposta_tcc.dataApresentacao = params[:proposta_tcc][:dataApresentacao]
-    @proposta_tcc.tcc.titulo = params[:titulo]
-    
 
     respond_to do |format|
       if @proposta_tcc.update(proposta_tcc_params)
@@ -81,6 +77,6 @@ class PropostaTccsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def proposta_tcc_params
       params.fetch(:proposta_tcc, {})
-      #params.require(:proposta_tcc).permit(:dataApresentacao, :aprovacao, :arquivo)
+      params.require(:proposta_tcc).permit(:dataApresentacao, :aprovacao, :arquivo, :aluno_id)
     end
 end
--- sigtcc/app/views/proposta_tccs/_proposta_tcc.json.jbuilder
+++ sigtcc/app/views/proposta_tccs/_proposta_tcc.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! proposta_tcc, :id, :titulo, :data, :created_at, :updated_at
+json.extract! proposta_tcc, :id, :created_at, :updated_at
 json.url proposta_tcc_url(proposta_tcc, format: :json)
