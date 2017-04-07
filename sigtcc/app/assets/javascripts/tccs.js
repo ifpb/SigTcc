492e71b456cd5d96dbd19f7095d5d98ddc5714bd
mayermatheus <matheusmayerduarte@gmail.com>
2017-04-07 01:00:57 -0300

    [UPDATE] - Adicionando id's para que o js puro exclua o input

--- sigtcc/app/assets/javascripts/tccs.js
+++ sigtcc/app/assets/javascripts/tccs.js
@@ -1,13 +1,18 @@
 $(document).on('turbolinks:load', function() {
   $('#add_palavras').click(function(event){
     event.preventDefault();
-    $(this).parent().before('<p><input type="text" name="palavras[]"> '+
-          '<a id="teste">Remover</a>');
+    $(this).parent().before('<p id="remove"><input type="text" name="palavras[]"> '+
+          '<span onclick="removerInput()" class="glyphicon glyphicon-remove" aria-hidden="true"></span></p>');
 
   });
 
-  $('#teste').click(function() {
-    console.log("chamou");
+  $('glyphicon glyphicon-remove').on('click', function() {
     $(this).parent().remove();
   })
 });
+
+function removerInput() {
+  var parent = document.getElementById('remove');
+  var parentePai = parent.parentNode;
+  parentePai.removeChild(parent);
+}
